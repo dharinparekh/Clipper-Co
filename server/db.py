72c2875ec1b59e3e24719e975f9bd7bc5362d660
Ankitesh Gupta <ankiteshguptas@gmail.com>
2017-03-25 22:39:15 +0530

    final commit for server file

--- server/db.py
+++ server/db.py
@@ -6,14 +6,19 @@ import pyotp
 import random
 
 
-USERNAME = 'root'
-HOST = '127.0.0.1'
-PASSWORD = 'tiger'
-DB = 'hackathon'
+USERNAME = 'XXX'
+HOST = 'xxx'
+PASSWORD = 'xxx'
+DB = 'xxx'
+
+SECRET = pyotp.random_base32()
 
 conn = MySQLdb.connect(HOST,USERNAME,PASSWORD,DB)
 conn.ping(True)
 ERROR = json.dumps({"status":False,"message":"mysql error"})
+htotp = pyotp.HOTP(SECRET)
+a = range(1,1000)
+
 
 #authenticate user
 def auth(uname,pwd):
@@ -50,3 +55,43 @@ def insert(uname,pwd):
             return ERROR
         return json.dumps({"status":True, "message":"user successfully entered"})
     # return json.dumps({"status":False, "message":"some error occured"})
+
+def _generate_otp(counter):
+    return htotp.at(int(counter))
+
+def _get_counter():
+    b = a[random.randint(0,len(a))]
+    a.remove(b)
+    return b
+
+def get_otp(uname,pwd):
+    counter = _get_counter()
+    otp = _generate_otp(counter)
+    query = "update users set counter= "+str(counter)+","+"otp = "+str(otp)+" where uname= '"+uname+"' and pwd= '"+pwd+"'"
+    curr = conn.cursor()
+    try:
+        curr.execute(query)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print e
+        return ERROR
+    return json.dumps({"status":True,"otp":otp})
+
+
+def check_otp(otp):
+    query = "select * from users where otp = "+str(otp)
+    curr = conn.cursor()
+    try:
+        curr.execute(query)
+    except Exception as e:
+        print e
+        return ERROR
+    data = curr.fetchall()
+    if len(data)!=1:
+        return json.dumps({"status":False,"topic":None,"message":"wrong otp"})
+    else:
+	#return  topic via joining username and pwd
+        topic = str(data[0][1])+'/'+str(data[0][2])+"/cc"
+        return json.dumps({"status":True,"topic":topic,"message":"matched"})
+    return ERROR
